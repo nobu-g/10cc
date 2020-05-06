2d3f89de2f37101c870e13c068fdd7ac9dc8d898
nobu-g <ueda-n127@hotmail.co.jp>
2020-05-06 16:02:17 +0900

    implement 4 arithmetic operations

--- 9cc.c
+++ 9cc.c
@@ -10,23 +10,6 @@ void print_header() {
     printf(".global main\n");
 }
 
-typedef enum {
-    TK_RESERVED,  // 記号
-    TK_NUM,       // 整数トークン
-    TK_EOF,       // 入力の終わりを表すトークン
-} TokenKind;
-
-typedef struct Token Token;
-
-struct Token {
-    TokenKind kind;
-    Token *next;
-    int val;
-    char *str;
-};
-
-// 現在着目しているトークン
-Token *token;
 char *user_input;
 
 void error_at(char *loc, char *fmt, ...) {
@@ -41,6 +24,32 @@ void error_at(char *loc, char *fmt, ...) {
     fprintf(stderr, "\n");
     exit(1);
 }
+
+typedef enum {
+    TK_RESERVED,  // 記号
+    TK_NUM,       // 整数トークン
+    TK_EOF,       // 入力の終わりを表すトークン
+} TokenKind;
+
+typedef enum {
+    ND_ADD,  // indicates +
+    ND_SUB,  // indicates -
+    ND_MUL,  // indicates *
+    ND_DIV,  // indicates /
+    ND_NUM,  // indicates a number
+} NodeKind;
+
+typedef struct Token Token;
+
+struct Token {
+    TokenKind kind;
+    Token *next;
+    int val;
+    char *str;
+};
+
+// 現在着目しているトークン
+Token *token;
 
 // returns True if the current token is op
 bool consume(char op) {
@@ -90,7 +99,7 @@ Token *tokenize(char *p) {
             continue;
         }
 
-        if (*p == '+' || *p == '-') {
+        if (*p == '+' || *p == '-' || *p == '*' || *p == '/' || *p == '(' || *p == ')') {
             cur = new_token(TK_RESERVED, cur, p++);
             continue;
         }
@@ -107,6 +116,100 @@ Token *tokenize(char *p) {
     return head.next;
 }
 
+typedef struct Node Node;
+
+struct Node {
+    NodeKind kind;
+    Node *lhs;
+    Node *rhs;
+    int val;
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = kind;
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+Node *new_node_num(int val) {
+    Node *node = calloc(1, sizeof(Node));
+    node->kind = ND_NUM;
+    node->val = val;
+    return node;
+}
+
+Node *expr();
+Node *mul();
+Node *primary();
+
+Node *expr() {
+    Node *node = mul();
+    for (;;) {
+        if (consume('+')) {
+            node = new_node(ND_ADD, node, mul());
+        } else if (consume('-')) {
+            node = new_node(ND_SUB, node, mul());
+        } else {
+            return node;
+        }
+    }
+}
+
+Node *mul() {
+    Node *node = primary();
+    for (;;) {
+        if (consume('*')) {
+            node = new_node(ND_MUL, node, primary());
+        } else if (consume('/')) {
+            node = new_node(ND_DIV, node, primary());
+        } else {
+            return node;
+        }
+    }
+}
+
+Node *primary() {
+    if (consume('(')) {
+        Node *node = expr();
+        expect(')');
+        return node;
+    }
+    return new_node_num(expect_number());
+}
+
+void gen(Node *node) {
+    if (node->kind == ND_NUM) {
+        printf("  push %d\n", node->val);
+        return;
+    }
+
+    gen(node->lhs);
+    gen(node->rhs);
+
+    printf("  pop rdi\n");
+    printf("  pop rax\n");
+
+    switch (node->kind) {
+    case ND_ADD:
+        printf("  add rax, rdi\n");
+        break;
+    case ND_SUB:
+        printf("  sub rax, rdi\n");
+        break;
+    case ND_MUL:
+        printf("  imul rax, rdi\n");
+        break;
+    case ND_DIV:
+        printf("  cqo\n");
+        printf("  idiv rdi\n");
+        break;
+    }
+
+    printf("  push rax\n");
+}
+
 int main(int argc, char **argv) {
     if(argc != 2) {
         fprintf(stderr, "%s: invalid number of arguments\n", argv[0]);
@@ -115,23 +218,12 @@ int main(int argc, char **argv) {
 
     user_input = argv[1];
     token = tokenize(argv[1]);  // first token of input
+    Node *node = expr();
 
     print_header();
     printf("main:\n");
-
-    printf("  mov rax, %d\n", expect_number());
-
-    while(!at_eof()) {
-        if (consume('+')) {
-            printf("  add rax, %d\n", expect_number());
-            continue;
-        }
-
-        expect('-');
-        printf("  sub rax, %d\n", expect_number());
-        continue;
-    }
-
+    gen(node);
+    printf("  pop rax\n");
     printf("  ret\n");
     return 0;
 }
--- test.sh
+++ test.sh
@@ -20,5 +20,8 @@ assert 0 0
 assert 42 42
 assert 42 "21+63-42"
 assert 42 "21 +  63 -   42"
+assert 47 "5+6*7"
+assert 15 "5*(9-6)"
+assert 4 "(3+5)/2"
 
 echo OK
