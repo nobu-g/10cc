54f7c75afeb5d418a635f6e5d1299ce0208f0d0d
nobu-g <ueda-n127@hotmail.co.jp>
2020-05-06 12:25:36 +0900

    implement addition and subtranction

--- 9cc.c
+++ 9cc.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char **argv) {
-  if (argc != 2) {
-    fprintf(stderr, "%s: invalid number of arguments\n", argv[0]);
-    return 1;
-  }
+void print_header() {
+    printf(".intel_syntax noprefix\n");
+    printf(".global main\n");
+}
 
-  printf(".intel_syntax noprefix\n");
-  printf(".global main\n");
-  printf("main:\n");
-  printf("  mov rax, %d\n", atoi(argv[1]));
-  printf("  ret\n");
-  return 0;
+int main(int argc, char **argv) {
+    if(argc != 2) {
+        fprintf(stderr, "%s: invalid number of arguments\n", argv[0]);
+        return 1;
+    }
+
+    char *p = argv[1];
+
+    print_header();
+    printf("main:\n");
+
+    printf("  mov rax, %ld\n", strtol(p, &p, 10));
+    while(*p) {
+        if(*p == '+') {
+            p++;
+            printf("  add rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+        if(*p == '-') {
+            p++;
+            printf("  sub rax, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+        fprintf(stderr, "Invalid character: '%c'\n", *p);
+        return 1;
+    }
+
+    printf("  ret\n");
+    return 0;
 }
