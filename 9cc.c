545d5eba27a8f3e6a466c30f8eb09ced949f48b3
nobu-g <ueda-n127@hotmail.co.jp>
2020-05-06 16:26:06 +0900

    add unary operator +, -

--- 9cc.c
+++ 9cc.c
@@ -142,6 +142,7 @@ Node *new_node_num(int val) {
 
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 Node *expr() {
@@ -158,15 +159,25 @@ Node *expr() {
 }
 
 Node *mul() {
-    Node *node = primary();
+    Node *node = unary();
     for (;;) {
         if (consume('*')) {
-            node = new_node(ND_MUL, node, primary());
+            node = new_node(ND_MUL, node, unary());
         } else if (consume('/')) {
-            node = new_node(ND_DIV, node, primary());
+            node = new_node(ND_DIV, node, unary());
         } else {
             return node;
         }
+    }
+}
+
+Node *unary() {
+    if (consume('+')) {
+        return primary();
+    } else if (consume('-')) {
+        return new_node(ND_SUB, new_node_num(0), primary());
+    } else {
+        return primary();
     }
 }
 
--- test.sh
+++ test.sh
@@ -23,5 +23,7 @@ assert 42 "21 +  63 -   42"
 assert 47 "5+6*7"
 assert 15 "5*(9-6)"
 assert 4 "(3+5)/2"
+assert 4 "-(3+5)/-(2)"
+assert 2 "-3+5/2 + 3"
 
 echo OK
